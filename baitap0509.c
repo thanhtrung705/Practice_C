b935e12c81d23761e7c9e8b299195b36e5c533e3
trung dinh <trungdtd00548@fpt.edu.vn>
2017-09-07 09:12:34 +0700

    Chỉnh sửa vài tập vòng lặp

--- baitap0509.c
+++ baitap0509.c
@@ -7,7 +7,8 @@ int add (int a,int b);
 int minus (int a,int b);
 int multi (int a,int b);
 int main() {
-    int a, b, option;
+    int a, b;
+    char option;
     while (true) {
         printf("1.Add 2 number\n");
         printf("2.Minus 2 number\n");
@@ -16,9 +17,9 @@ int main() {
         printf("5.Exit\n");
         printf("________________________\n");
         printf("Enter a number: \n");
-        scanf("%d", &option);
+        scanf("%c", &option);
             switch (option) {
-                case 1:
+                case '1':
                     printf("Plus 2 number\n");
                     printf("Please enter a=");
                     scanf("%d", &a);
@@ -26,7 +27,7 @@ int main() {
                     scanf("%d", &b);
                     printf("Sum is %d\n", add(a, b));
                     break;
-                case 2:
+                case '2':
                     printf("Minus 2 number\n");
                     printf("Please enter a=");
                     scanf("%d", &a);
@@ -34,18 +35,19 @@ int main() {
                     scanf("%d", &b);
                     printf("Difference is %d\n", minus(a, b));
                     break;
-                case 3:
+                case '3':
                     printf("Divide 2 number\n");
                     printf("Please enter a=");
                     scanf("%d", &a);
-                    printf("Please enter b=");
-                    scanf("%d", &b);
-                    if(b!=0)
-                        printf("Quotient is %f\n", divi(a, b));
-                    else
-                        printf("b cannot receive value 0\n");
+                    do {
+                        printf("Please enter b=");
+                        scanf("%d", &b);
+                        if (b == 0)
+                            printf("b cannot receive value '0'\nInput a valid number\n");
+                    }while(b==0);
+                        printf("Quotient is %f\n", divi(a,b));
                     break;
-                case 4:
+                case '4':
                     printf("Multiply 2 number\n");
                     printf("Please enter a=");
                     scanf("%d", &a);
@@ -53,13 +55,13 @@ int main() {
                     scanf("%d", &b);
                     printf("Product is %d\n", multi(a, b));
                     break;
-                case 5:
+                case '5':
                     break;
                 default:
                     printf("Please enter number again!\n");
                     break;
             }
-        if (option == 5)
+        if (option == '5')
             break;
         }
     }
